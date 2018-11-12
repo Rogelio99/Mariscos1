d0ab56c42af817cc0f0727c0162f867b37954824
OsalRami <osalrami.97@gmail.com>
2018-11-11 23:05:35 -0600

    oli
    nadamas una cosa el la base de datos elimina la tabla cliente y agrega esta:
    CREATE TABLE IF NOT EXISTS `users` (
      `id` int(11) AUTO_INCREMENT NOT NULL,
      `username` varchar(50) NOT NULL,
      `email` varchar(80) NOT NULL,
      `password` varchar(250) NOT NULL,
      `authKey` varchar(250) NOT NULL,
      `accessToken` varchar(250) NOT NULL,
      `activate` tinyint(1) NOT NULL DEFAULT '0',
      PRIMARY KEY (`id`)
    );

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mariscos',
+    'dsn' => 'mysql:host=localhost;dbname=Mariscos1',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
